cd8e2e7c7c262a8af248a86bf5f6ce427b38ded1
jim <jim@crespirit.com>
2021-01-09 09:02:47 +0800

    close

--- src/components/Gifts.js
+++ src/components/Gifts.js
@@ -2,10 +2,7 @@ import React, { useRef } from "react";
 import styles from "./gifts.module.css";
 
 import { FormattedMessage } from "react-intl";
-// import video_en from "../public/assets/videos/video_en.mp4";
-// import video_zh from "../public/assets/videos/video_zh.mp4";
-// import poster_en from "../public/assets/images/poster_en.png";
-// import poster_zh from "../public/assets/images/poster_zh.png";
+
 
 const Gifts = (props) => {
   return (
@@ -21,20 +18,9 @@ const Gifts = (props) => {
         <p><FormattedMessage id="app.p2-p9-4" /></p>
         <p className="subtitle_p1"><FormattedMessage id="app.p2-p9-5" /></p>
         <p><FormattedMessage id="app.p2-p9-6" /></p>
-
-        {/* <div className={styles.giftWrapper}>
-          <video preload="auto" className={props.lang.includes("zh") ? `${styles.show}` : `${styles.hide}`} controls poster={poster_zh} controlsList="nodownload">
-            <source src={video_zh} type="video/mp4" />
-          </video>
-          <video preload="auto" className={props.lang.includes("en") ? `${styles.show}` : `${styles.hide}`} controls poster={poster_en} controlsList="nodownload">
-            <source src={video_en} type="video/mp4" />
-          </video>
-        </div> */}
       </div>
     </div>
   );
 };
 
 export default Gifts;
-
-// 增加打x
