288df8a2ff591565b6f64d5828a70668b50e2bfd
jim <jim@crespirit.com>
2021-01-20 08:57:47 +0800

    temp.

--- src/components/Winner.js
+++ src/components/Winner.js
@@ -5,7 +5,7 @@ import styles from "./winner.module.css";
 import image from "../public/assets/images/members/25.png";
 const judges = ["4 NUDA", "14 Nano ka", "18 ike", "49	風船貓", "59 Lee", "66 映はち"];
 const popular = ["15 霧鈴桐喻", "67 プチ", "1 Zavok", "51 Losno", "56 saltywetspirit 鹹濕精神", "45 世子"];
-const member = ["takumi_7**@yahoo.com", "shaoyou**@gmail.com", "wcko**@gmail.com", "taikokid**@gmail.com", "caffeinefightersha**@gmail.com", "seanhunter**@gmail.com", "six.shooter0**@gmail.com", "kenny742**@gmail.com", "kirablahete**@gmail.com", "a826239**@gmail.com", "matheussouza01**@gmail.com", "knock36**@yahoo.co.jp", "pshx1**@gmail.com", "pshx1**@gmail.com", "4907517**@qq.com", "samca1**@gmail.com", "darkm**@gmail.com", "garyfong**@gmail.com", "themultitasker98**@gmail.com", "gapt427820**@yahoo.co.jp"];
+const member = ["takumi_7**@yahoo.com", "shaoyou**@gmail.com", "wcko**@gmail.com", "taikokid**@gmail.com", "caffeinefightersha**@gmail.com", "seanhunter**@gmail.com", "six.shooter0**@gmail.com", "kenny742**@gmail.com", "kirablahete**@gmail.com", "a826239**@gmail.com", "matheussouza01**@gmail.com", "knock36**@yahoo.co.jp", "pshx1**@gmail.com", "ely05**@hotmail.com", "4907517**@qq.com", "samca1**@gmail.com", "darkm**@gmail.com", "garyfong**@gmail.com", "themultitasker98**@gmail.com", "gapt427820**@yahoo.co.jp"];
 const special = ["26 s.Claw"];
 
 const Winner = () => {
